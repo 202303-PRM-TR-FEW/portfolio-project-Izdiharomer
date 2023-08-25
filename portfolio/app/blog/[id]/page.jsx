ac81ec97f869acdb47f1718012532bdacfecb690
Izdihar <ezdiharomerrrr@gmail.com>
2023-08-25 16:13:26 +0300

    fix: style of backgound image updated_

--- portfolio/app/blog/[id]/page.jsx
+++ portfolio/app/blog/[id]/page.jsx
@@ -25,7 +25,7 @@ const Page = () => {
   return (
     <div className='w-full' key={project.id}>
       <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/50 z-10' />
         <Image
           className='absolute z-1'
           layout='fill'
