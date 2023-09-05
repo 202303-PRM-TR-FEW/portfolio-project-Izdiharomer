972a65373c638ec3114ec0478a8de98147c18da4
Izdihar <ezdiharomerrrr@gmail.com>
2023-09-05 19:31:02 +0300

    refactor: projects were updated

--- portfolio/app/components/ProjectItem.jsx
+++ portfolio/app/components/ProjectItem.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 import Link from 'next/link';
 import projects from '../data/projects.json';
 
-const ProjectItem = ({ id, title, imageUrl, tech }) => {
-  //const project = projects.projects.find((p) => p.id === parseInt(id));
-  // const project = projects.projects.find((p) => p.id === 1);
-
-  // // if (!project) {
-  // //   return <div>No project found with id {id}</div>;
-  // // }
+const ProjectItem = ({ id, title, imageUrl, tech, liveDemoUrl, description, githubUrl}) => {
+  
 
   return (
-    <div className='relative flex justify-center  shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#07beb8] to-[#3dccc7]'>
+    <div className='relative flex justify-center m-2 shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#07beb8] to-[#3dccc7]'>
       <Image
         className='rounded-xl group-hover:opacity-10 w-[405px] h-[220px]'
         src={imageUrl}
@@ -21,17 +16,31 @@ const ProjectItem = ({ id, title, imageUrl, tech }) => {
         height={220}
         priority
       />
-      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full m-0 p-1'>
         <h3 className='text-1xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt-2 text-sm text-white tracking-wider text-center'>
+        <p className=' pt-2 text-sm text-white opacity-90 text-center'>
           {tech}
         </p>
-        {/* Use Link to create a dynamic link */}
-        <Link href={`/project/${id}`}>
-          <p className='text-center py-1 m-1 rounded-lg bg-white text-gray-700 font-bold text-sm cursor-pointer'>
-            <span className='px-3 py-2'>More Info</span>
-          </p>
-        </Link>
+        <p className='text-xs py-1 p-1 text-white w-full text-center '>
+          {description}
+        </p>
+        <div className='flex justify-center items-center gap-2'>
+        <a
+            href={liveDemoUrl}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-1 mt-2 pink_button'>Live</button>
+          </a>
+          <a
+            href={githubUrl}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-1 mt-2 pink_button'>code</button>
+          </a>
+          </div>
+      
       </div>
     </div>
   );
